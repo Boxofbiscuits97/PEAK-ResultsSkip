5791677ded63877ccee8fb7c04b7a1300daf6a00
BiscuitDev <86505354+Boxofbiscuits97@users.noreply.github.com>
2025-08-25 19:25:13 -0400

    first commit

---
+++ src/ResultsSkip/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Photon.Pun;
+using Zorro.Core;
+
+namespace ResultsSkip;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+
+        Log.LogInfo($"Plugin {Name} is loaded!");
+        Harmony.CreateAndPatchAll(typeof(ResultsSkip));
+    }
+    
+}
+
+public class ResultsSkip {
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(EndScreen), nameof(EndScreen.Next))]
+    private static void ResultsSkipPatch() {
+        if (!PhotonNetwork.IsMasterClient) {
+            Plugin.Log.LogWarning("Only the Master Client can skip results!");
+            return;
+        }
+        Singleton<PeakHandler>.Instance.EndScreenComplete();
+        Plugin.Log.LogInfo("Skipped Results Screen for all players.");
+    }
+}
